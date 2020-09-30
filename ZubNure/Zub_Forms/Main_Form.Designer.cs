5966c57a96aa775339e5173703f64a521e411f2c
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2020-09-30 14:36:13 +0300

    Micro-change 2
    dao and dentistry back

--- ZubNure/Zub_Forms/Main_Form.Designer.cs
+++ ZubNure/Zub_Forms/Main_Form.Designer.cs
@@ -48,9 +48,10 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.panel1.ForeColor = System.Drawing.Color.Green;
-            this.panel1.Location = new System.Drawing.Point(12, 4);
+            this.panel1.Location = new System.Drawing.Point(16, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(769, 70);
+            this.panel1.Size = new System.Drawing.Size(1025, 86);
             this.panel1.TabIndex = 0;
             // 
             // label2
@@ -58,27 +59,30 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(495, 44);
+            this.label2.Location = new System.Drawing.Point(660, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(172, 17);
+            this.label2.Size = new System.Drawing.Size(205, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Телефон : 095 67 786 45";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(82, 5);
+            this.label1.Location = new System.Drawing.Point(109, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(487, 31);
+            this.label1.Size = new System.Drawing.Size(613, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "Стоматологическая клиника ZubNure";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(699, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(932, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(61, 58);
+            this.pictureBox1.Size = new System.Drawing.Size(81, 71);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -86,9 +90,10 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
             this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(12, 205);
+            this.panel2.Location = new System.Drawing.Point(16, 252);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(421, 217);
+            this.panel2.Size = new System.Drawing.Size(561, 267);
             this.panel2.TabIndex = 1;
             // 
             // button1
@@ -96,22 +101,24 @@
             this.button1.BackColor = System.Drawing.Color.Maroon;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(88, 87);
+            this.button1.Location = new System.Drawing.Point(117, 107);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 67);
+            this.button1.Size = new System.Drawing.Size(304, 82);
             this.button1.TabIndex = 0;
             this.button1.Text = "Записатся к доктору";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // Main_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(784, 434);
+            this.ClientSize = new System.Drawing.Size(945, 525);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main_Form";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
