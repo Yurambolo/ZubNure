48baf56eca0e01c80b234a2bf5bb4d048b88e3cc
avojarot <64825394+avojarot@users.noreply.github.com>
2020-10-04 13:56:58 +0300

    new

--- ZubNure/Zub_Forms/Doctrors_Form.Designer.cs
+++ ZubNure/Zub_Forms/Doctrors_Form.Designer.cs
@@ -232,9 +232,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.Location = new System.Drawing.Point(3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(58, 22);
             this.label3.TabIndex = 0;
             this.label3.Text = "label3";
             // 
